9315e494496e407261579d9a483bc120a6524ab4
pxlmastr <calebwietholter9@outlook.com>
2025-05-01 13:14:34 +0000

    Changed database to SQLite, due to issues using postgres.

--- operations/index.ts
+++
@@ -1,8 +0,0 @@
-import client from "./postgres";
-
-async function test() {
-    await client.connect();
-    
-}
-
-export default test;
--- operations/postgres.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var postgres = require('postgres');
-var connectionString = "postgresql://postgres:@zw#J@BL7ZK7g&6@db.saiqmuffyffcigmisyxa.supabase.co:5432/postgres";
-var sql = postgres(connectionString);
-exports.default = sql;
